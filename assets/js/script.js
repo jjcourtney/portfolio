45e4b840291a02812c8534a0ffe273e14f27c02d
jjcourtney <jcourtneywork@gmail.com>
2021-07-29 14:32:01 +0100

    added blur effect + other styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,61 @@
-class PortfolioLink {
-    constructor(projectObj){
+// class PortfolioLink {
+//     constructor(projectObj){
 
-        const {url, image, languages, isFeature} = projectObj;
-        Object.assign(this, {url, image, languages, isFeature});
-    }
-    get elements() {
-        this.createSection()
+//         const {url, image, languages, isFeature} = projectObj;
+//         Object.assign(this, {url, image, languages, isFeature});
+//     }
+//     get elements() {
+//         this.createSection()
+//         this.createDivs()
+//         return this.sectionEl;
+//     }
+
+//     createSection() {
+//         this.sectionEl = $("<section>")
+//                         .addClass("columns-card");
+//         if(this.isFeature){
+//             this.sectionEl.attr("id", "feature");
+//         }
+//     }
+//     createDivs() {
+//         this.imgDiv = $("<div>")
+//         .addClass("card-img")
+//         .append($(`<a href="${this.url}" target="blank"><img src="${this.image}" class="link-img">`))
+//         this.langDiv = $(`<div class="card-text">${this.languages}</div>`);
+
+//         this.sectionEl.append(this.imgDiv);
+//         this.sectionEl.append(this.langDiv);
         
-        return this.sectionEl;
-    }
+//     }
 
-    createSection() {
-        this.sectionEl = $("<section>")
-                        .addClass("columns-card")
-                        .text(`${this.url}`);
-        if(this.isFeature){
-            this.sectionEl.attr("id", "feature");
-        }
-    }
-    createDivs() {
-        this.imgDiv = $("<div>")
-        .addClass("card-img")
-        .attr
-        this.sectionEl.append()
-    }
+// }
 
-}
+// const objArr = [{name: "test", url: "some.com", image: "./assets/images/periodic.png", languages : "HTML", isFeature: true},
+//                 {name: "test2", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test3", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test4", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test5", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test6", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test7", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test8", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test9", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false},
+//                 {name: "test10", url: "thing.com", image: "./assets/images/periodic.png", languages : "HTML/PHP/CSS", isFeature: false}]
 
-const objArr = [{name: "test", url: "some.com", image: "/image.png", languages : "HTML", isFeature: true},
-                {name: "test2", url: "thing.com", image: "/image.png", languages : "HTML", isFeature: false}]
+// const testDiv = $("#rows-cards");
 
-const testDiv = $("#div-test");
+// objArr.forEach((linkObject, i) =>{
+//     let someName = linkObject.name
+//     this[someName] =  new PortfolioLink(objArr[i]);
+//     testDiv.append(this[someName].elements)
+// })
 
-objArr.forEach((linkObject, i) =>{
-    let someName = linkObject.name
-    this[someName] =  new PortfolioLink(objArr[i]);
-})
 
-testDiv.append(test.elements);
-testDiv.append(test2.elements);
+
+
+$(".can-remove-blur").hover(event => {
+    thisElementsID = event.target.id;
+    $(`#${thisElementsID}`).removeClass("blur-sm");
+}, event =>{
+    thisElementsID = event.target.id;
+    $(`#${thisElementsID}`).addClass("blur-sm");
+});
